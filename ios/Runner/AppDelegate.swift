fd42ce019784dc9fd11a9a7692aa072026c340ae
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-12-17 11:59:49 +0200

    apply google map configuration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
+@UIApplicationMain
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCH5xkUIDelmviGIyWqEnHeRpdOVT6W_L4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/layout/home_view.dart
+++ lib/layout/home_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(29.015670998478736, 30.445375323550063))),
+    );
   }
 }
